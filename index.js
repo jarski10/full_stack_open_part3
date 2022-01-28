498e89bcac6a70b7cb6932999f903b24d0dffebd
Jarkko Passi <jarkko.passi@tuni.fi>
2022-01-28 14:41:13 +0200

    Change port for heroku

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
+//const PORT = process.env.PORT || 3001
+app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
