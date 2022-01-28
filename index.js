65c7c894be31c82c4076bb5028eb7ef2e12b4a25
Jarkko Passi <jarkko.passi@tuni.fi>
2022-01-28 15:35:52 +0200

    uibuild

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const cors = require('cors')
 
 const app = express()
 
-const API = process.env.NODE_ENV === 'production' ? 'https://yourapp.herokuapp.com/api' : 'http://localhost:5000/api';
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan((tokens, req, res) => {
     return [
