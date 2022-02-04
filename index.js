0d66989419721c7eb1bd01f3ec1f882f4b1d3c45
Jarkko Passi <jarkko.passi@tuni.fi>
2022-02-04 12:03:26 +0200

    uibuild

--- index.js
+++ index.js
@@ -26,10 +26,6 @@ app.use(morgan((tokens, req, res) => {
 })
 )
 
-
-
-
-
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons => {
         res.json(persons)
@@ -66,25 +62,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-
-    if (!body.number) {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-
-    // if (persons.find(person => person.name === body.name)) {
-    //     return response.status(400).json({
-    //         error: 'Name already in list'
-    //     })
-    // }
-
+    
     const person = new Person({
         name: body.name,
         number: body.number,
@@ -103,6 +81,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
 
--- models/person.js
+++ models/person.js
@@ -12,8 +12,16 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    number: {
+        type: String,
+        required: true,
+        minlength: 3
+    }
 })
 
 personSchema.set('toJSON', {
